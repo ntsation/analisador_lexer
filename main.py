db08fc001d0da7c50fa19e3c934f6d386f23cc55
Nathan Souza <naathanmarinho11@gmail.com>
2024-05-25 13:37:17 -0300

    adiciona melhorias ao codigo

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import re
 
 patterns = [
-    (r'[a-zA-Z_]\w*', 'IDENTIFIER'),  
-    (r'(if|else|for|while|return)\b', 'KEYWORD'),  
-    (r'[\+\-\*/=]', 'OPERATOR'),  
-    (r'[(),;{}]', 'SYMBOL'),  
-    (r'\d+', 'NUMBER'),  
-    (r'//.*|/\*[\s\S]*?\*/', ''),  
-    (r'\s+', ''),  
+    (r'[a-zA-Z_]\w*', 'IDENTIFIER'),  # Identificador
+    (r'(if|else|for|while|return)\b', 'KEYWORD'),  # Palavras-chave
+    (r'[\+\-\*/=<>]', 'OPERATOR'),  # Operadores
+    (r'[(),;{}]', 'SYMBOL'),  # Símbolos
+    (r'\d+', 'NUMBER'),  # Números
+    (r'"(?:\\.|[^"\\])*"', 'STRING'), # Strings
+    (r'//.*', ''),  # Comentários de linha
+    (r'/\*[\s\S]*?\*/', ''), # Comentários de bloco
+    (r'\s+', ''),  # Espaços em branco
 ]
 
+# Exemplo de código
 def lexer(code):
     tokens = []
     while code:
@@ -21,13 +24,20 @@ def lexer(code):
                     tokens.append((value, token_type))
                 code = code[len(value):]
                 break
+        else:
+            raise ValueError("Caractere inesperado: {}".format(code[0]))
     return tokens
 
+# Teste
 code = """
 int main() {
     // Programa de exemplo
     int x = 5;
+    if (x > 0) {
+        return 1;
+    }
     return 0;
 }
 """
+
 print(lexer(code))
